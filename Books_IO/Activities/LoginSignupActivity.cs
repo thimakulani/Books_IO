b1c1316675e62cad800ababb5e46704606cb6533
Thima <thimakulani@gmail.com>
2021-05-30 14:52:49 +0200

    Login and sign up host activity

---
+++ Books_IO/Activities/LoginSignupActivity.cs
@@ -0,0 +1,24 @@
+﻿using Android.App;
+using Android.Content;
+using Android.OS;
+using Android.Runtime;
+using Android.Views;
+using Android.Widget;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Books_IO.Activities
+{
+    [Activity(Label = "LoginSignupActivity")]
+    public class LoginSignupActivity : Activity
+    {
+        protected override void OnCreate(Bundle savedInstanceState)
+        {
+            base.OnCreate(savedInstanceState);
+
+            // Create your application here
+        }
+    }
+}
